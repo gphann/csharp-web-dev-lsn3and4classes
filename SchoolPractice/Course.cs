b0a337e838b56c23c903146a48613a695cccc766
Gloria <hklover@gmail.com>
2020-12-07 16:50:58 -0600

    added files course & teacher

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+
+        private string Name { get; set; }
+        private int Number { get; set; }
+        private List<Student> StudentList = new List<Student>();
+
+
+    }
+}
---
+++ SchoolPractice/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Teacher
+    {
+        private string firstName;
+        private string lastName;
+        private string subject;
+        private double yearsTeaching;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+        public double YearsTeaching { get; set; }
+
+    }
+}
